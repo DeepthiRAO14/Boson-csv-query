e14e0143440caefaaea08c45c57198fe4f78fe81
DeepthiRAO14 <158045617+DeepthiRAO14@users.noreply.github.com>
2024-01-30 23:43:35 +0530

    query.py

--- query.py
+++ query.py
@@ -36,19 +36,6 @@ def main():
             # Display HTML table
             st.markdown(data_html, unsafe_allow_html=True)
 
-            # Add a fixed image at the top right
-            image_path = "https://github.com/DeepthiRAO14/Boson-csv-query/blob/main/Bosonlogo.png"  # Replace with the actual filename and extension
-            image_url = f"data:image/png;base64,{base64.b64encode(open(image_path, 'rb').read()).decode()}"
-            response = requests.get(image_url)
-
-        if response.status_code == 200:
-            image_content = base64.b64encode(response.content).decode()        
-            st.markdown(
-                f'<style>div.stImage img {{ float: right; }}</style>'
-                f'<div style="position: fixed; top: 10px; right: 10px;"><img src="{image_url}" alt="Image" width="100"></div>',
-                unsafe_allow_html=True
-            )
-
         else:
             st.warning("File is None. Please upload a CSV file.")
     except Exception as e:
